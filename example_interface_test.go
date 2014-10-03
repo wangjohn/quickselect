aa01b4241a5a62a158bf1c7af450326c916aaad9
wangjohn <wangjohn@mit.edu>
2014-10-02 23:52:52 -0700

    Remove extra array declaration.

--- example_interface_test.go
+++ example_interface_test.go
@@ -19,8 +19,6 @@ func (a ByAge) Len() int      { return len(a) }
 func (a ByAge) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a ByAge) Less(i, j int) { return a[i].Age < a[j].Age }
 
-integerArray := []IntSlice{10, 5, 3, 15, 4, 9, 3, 4}
-
 func Example() {
   people := []Person{
       {"Bob", 31},
